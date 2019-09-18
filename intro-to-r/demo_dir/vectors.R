02c674dc00c0d6491afd60aac887e993e1be7820
Sawyer Newman <sauuyer@gmail.com>
2019-09-18 09:25:58 -0400

    Updated vectors.R

--- intro-to-r/demo_dir/vectors.R
+++ intro-to-r/demo_dir/vectors.R
@@ -8,9 +8,16 @@ seq1 <- seq(from = 1, to = 3000, by = 100)
 seq(from = 0, to = 50, by = .5)
 
 ############ LISTS
-samples <- c('ts001', 'ts4938', 'ts9999', 'ts1', 'ts2')
-responses <- c(1, 1, 1, 2, 3, 4, 10)
+samples <- c('ts3', 'ts2', 'ts1', 'ts1', 'ts2')
+responses <- c(100, 200, 300, 400, 500, 600)
+other_list <- c(1, 2, 3, 4, 5, 6)
+short_list <- c(1,2)
+
+responses * 2
+responses + other_list
+responses + short_list
 
 ############ VECTOR FUNCTIONS
 sort(samples)
-unique(responses)
+unique(samples)
+
